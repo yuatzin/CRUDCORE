26447420fc07c76ca2fbe1026e2a4fa65d718ad9
yuatzin <78455999+yuatzin@users.noreply.github.com>
2024-10-26 13:45:20 -0600

    Agregar archivos de proyecto.

---
+++ CRUDCORE/Datos/ContactoDatos.cs
@@ -0,0 +1,169 @@
+﻿using CRUDCORE.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CRUDCORE.Datos
+{
+    public class ContactoDatos
+    {
+        public List<ContactoModel> Listar()
+        {
+
+            var oLista = new List<ContactoModel>();
+
+            var cn = new Conexion();
+
+            using (var conexion = new SqlConnection(cn.getCadenaSQL()))
+            {
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand("sp_Listar", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader())
+                {
+
+                    while (dr.Read())
+                    {
+                        oLista.Add(new ContactoModel()
+                        {
+                            matricula = Convert.ToInt64(dr["matricula"]),
+                            nombre = dr["nombre"].ToString(),
+                            telefono = dr["telefono"].ToString(),
+                            correo = dr["correo"].ToString()
+                        });
+
+                    }
+                }
+            }
+
+            return oLista;
+        }
+
+        public ContactoModel Obtener(int matricula)
+        {
+
+            var oContacto = new ContactoModel();
+
+            var cn = new Conexion();
+
+            using (var conexion = new SqlConnection(cn.getCadenaSQL()))
+            {
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand("sp_Obtener", conexion);
+                cmd.Parameters.AddWithValue("matricula", matricula);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader())
+                {
+
+                    while (dr.Read())
+                    {
+                        oContacto.matricula = Convert.ToInt64(dr["matricula"]);
+                        oContacto.nombre = dr["nombre"].ToString();
+                        oContacto.telefono = dr["telefono"].ToString();
+                        oContacto.correo = dr["correo"].ToString();
+                    }
+                }
+            }
+
+            return oContacto;
+        }
+
+        public bool Guardar(ContactoModel ocontacto)
+        {
+            bool rpta;
+
+            try
+            {
+                var cn = new Conexion();
+
+                using (var conexion = new SqlConnection(cn.getCadenaSQL()))
+                {
+                    conexion.Open();
+                    SqlCommand cmd = new SqlCommand("sp_Guardar", conexion);
+                    cmd.Parameters.AddWithValue("matricula", ocontacto.matricula);
+                    cmd.Parameters.AddWithValue("nombre", ocontacto.nombre);
+                    cmd.Parameters.AddWithValue("telefono", ocontacto.telefono);
+                    cmd.Parameters.AddWithValue("correo", ocontacto.correo);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                rpta = true;
+
+
+            }
+            catch (Exception e)
+            {
+
+                string error = e.Message;
+                rpta = false;
+            }
+
+
+
+            return rpta;
+        }
+
+
+        public bool Editar(ContactoModel ocontacto)
+        {
+            bool rpta;
+
+            try
+            {
+                var cn = new Conexion();
+
+                using (var conexion = new SqlConnection(cn.getCadenaSQL()))
+                {
+                    conexion.Open();
+                    SqlCommand cmd = new SqlCommand("sp_Editar", conexion);
+                    cmd.Parameters.AddWithValue("matricula", ocontacto.matricula);
+                    cmd.Parameters.AddWithValue("nombre", ocontacto.nombre);
+                    cmd.Parameters.AddWithValue("telefono", ocontacto.telefono);
+                    cmd.Parameters.AddWithValue("correo", ocontacto.correo);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                rpta = true;
+
+
+            }
+            catch (Exception e)
+            {
+
+                string error = e.Message;
+                rpta = false;
+            }
+            return rpta;
+        }
+
+        public bool Eliminar(int matricula)
+        {
+            bool rpta;
+
+            try
+            {
+                var cn = new Conexion();
+
+                using (var conexion = new SqlConnection(cn.getCadenaSQL()))
+                {
+                    conexion.Open();
+                    SqlCommand cmd = new SqlCommand("sp_Eliminar", conexion);
+                    cmd.Parameters.AddWithValue("matricula", matricula);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                rpta = true;
+
+
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+                rpta = false;
+            }
+            return rpta;
+        }
+
+    }
+}
---
+++ CRUDCORE/Models/ContactoModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDCORE.Models
+{
+    public class ContactoModel
+    {
+        
+        public long matricula { get; set; }
+
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
+        public string? nombre { get; set; }
+        [Required(ErrorMessage = "El campo Telefono es obligatorio")]
+        public string? telefono { get; set; }
+        [Required(ErrorMessage = "El campo Correo es obligatorio")]
+        public string? correo { get; set; }
+    }
+}
